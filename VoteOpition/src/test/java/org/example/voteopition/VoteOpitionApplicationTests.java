a1d0d167169aa5f372b16cdfa1a46be17c97db0a
anis6677 <askoey@hotmail.com>
2024-09-10 13:47:34 +0200

    nesten fullført

---
+++ VoteOpition/src/test/java/org/example/voteopition/VoteOpitionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.voteopition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteOpitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
